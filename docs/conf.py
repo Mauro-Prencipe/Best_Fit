4473c9d5bd799922fb1559695c348c433be3478d
Mauro-Prencipe <49814184+Mauro-Prencipe@users.noreply.github.com>
2021-03-29 16:33:59 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import matplotlib
 import pandas
 import scipy
 import IPython
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../', '.', './source'))
 
 
 # -- Project information -----------------------------------------------------
